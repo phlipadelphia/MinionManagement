c624e86d3d383fc4fc267f1f29e350fa4a03f809
Zephyr <phillip.t.mock@gmail.com>
2022-06-07 18:07:10 -0700

    seeds and sql code

---
+++ db/queries.sql
@@ -0,0 +1,5 @@
+USE minion_db
+
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+USE minions_db
+
+INSERT INTO departments (department_name)
+VALUES
+    ("R & D")
+    ("Accounting")
+    ("Production")
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('CEO', 1200000, 1),
+    ('Minion Wrangler', 70000, 2),
+    ('Back End Minion', 60000, 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Bob', 'Edith', 1, 1),
+    ('Dave', 'Margo', 2, 7),
+    ('Stuart', 'Agnes', 3, 9);
